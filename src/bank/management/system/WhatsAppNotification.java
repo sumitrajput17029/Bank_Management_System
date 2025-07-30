d38e3e8b706e372cd4146d68c98825d6c87bf4e7
Sumit Kumar <sumitrajput17029@gmail.com>
2025-07-30 20:55:27 +0530

    Update WhatsAppNotification.java

--- src/bank/management/system/WhatsAppNotification.java
+++ src/bank/management/system/WhatsAppNotification.java
@@ -5,12 +5,12 @@ import com.twilio.rest.api.v2010.account.Message;
 import com.twilio.type.PhoneNumber;
 
 public class WhatsAppNotification {
-    private static final String ACCOUNT_SID = "AC8ae30525ac357c7e848513588a374024";
-    private static final String AUTH_TOKEN = "2166a9fed1721e1874bcae4241490d03";
+    private static final String ACCOUNT_SID = "AC8ae30525ac357c7e848513588________"; // fill generate the twilio api 
+    private static final String AUTH_TOKEN = "2166a9fed1721e1874bcae424___________";   // fill generate the twilio api 
     
  // Twilio numbers
-    private static final String TWILIO_SMS_NUMBER = "+12175664216"; 
-    private static final String TWILIO_WHATSAPP_NUMBER = "whatsapp:+14155238886"; 
+    private static final String TWILIO_SMS_NUMBER = "+1217566_____"; 
+    private static final String TWILIO_WHATSAPP_NUMBER = "whatsapp:+141552______";  //fill okay 
 
     static {
         Twilio.init(ACCOUNT_SID, AUTH_TOKEN);
